245c247deecc8abfcc478213e021e9567ffc0999
stephHiyagon <mv.hiyagon@gmail.com>
2017-05-26 12:17:47 -0500

    autocomplete and route

---
+++ js/app.js
@@ -0,0 +1,69 @@
+  function initMap() {
+  var laboratoriaLima = {lat: -12.1191427, lng: -77.0349046};
+  var map = new google.maps.Map(document.getElementById("map"),{
+    zoom: 18,
+		center: laboratoriaLima
+  });
+
+  var marcadorLaboratoria = new google.maps.Marker({
+    position: laboratoriaLima,
+    map: map
+  });
+
+  function buscar() {
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(exito,error);
+    }
+  }
+  var inputPartida=document.getElementById("partida");
+  var inputDestino=document.getElementById("destino");
+
+  new google.maps.places.Autocomplete(inputPartida);
+  new google.maps.places.Autocomplete(inputDestino);
+
+  var directionsService= new google.maps.DirectionsService;
+  var directionsDisplay=new google.maps.DirectionsRenderer;
+
+  var calculateAndDisplayRoute = function(directionsService,directionsDisplay){
+    directionsService.route({
+      origin:inputPartida.value,
+      destination:inputDestino.value,
+      travelMode:'DRIVING'
+    },function (response,status){
+      if(status==='OK'){
+        directionsDisplay.setDirections(response);
+      }else{
+        window.alert("No encontramos la ruta");
+      }
+    });
+  }
+
+  directionsDisplay.setMap(map);
+
+  var trazarRuta=function(){
+    calculateAndDisplayRoute(directionsService,directionsDisplay);
+  }
+
+  document.getElementById('trazar').addEventListener('click',trazarRuta);
+
+  document.getElementById("encuentrame").addEventListener("click",buscar);
+
+	var latitud,longitud;
+	var exito = function(posicion) {
+		latitud = posicion.coords.latitude;
+		longitud = posicion.coords.longitude;
+
+	var miUbicacion = new google.maps.Marker({
+			position: {lat:latitud, lng:longitud},
+			animation: google.maps.Animation.DROP,
+			map: map
+		});
+		map.setZoom(18);
+		map.setCenter({lat:latitud, lng:longitud});
+	}
+
+	var error = function (error) {
+		alert("Tenemos un problema con encontrar tu ubicación");
+	}
+
+}
